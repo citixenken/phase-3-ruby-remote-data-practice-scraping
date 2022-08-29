53d22a0d97963ee079235695c7223f90032e05ba
Ken Muyesu <ken.muyesu@student.moringaschool.com>
2022-08-29 07:04:02 +0300

    commit tree 4d1a7af8a7dcc08932db60c05d82c5b55ff7592f

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,16 @@
-require 'nokogiri'
-require 'open-uri'
+require 'nokogiri' #allows us to parse HTML and collect data from it
+require 'open-uri' #allows us to programatically make HTTP requests
 
-html = open("https://flatironschool.com/")
+html = URI.open("https://flatironschool.com/")
+# html = URI.open("https://bookhub-app.netlify.app/")
+# html = URI.open("https://www.codingkenya.com/")
+
+# puts html
+
+#converting HTML returned into a NodeSet => nested nodes(tree elements containing parent and children elements)
+doc = Nokogiri::HTML(html)
+# puts doc.css('h1.heading-financier').text.strip #cleanup extra whitespace
+# puts doc.css('div.card-body > h3').text
+courses = doc.css('div.card-body > h3')
+
+courses.map { |course| puts course.text.strip }
